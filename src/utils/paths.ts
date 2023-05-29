65e4a7d0593368e3e0a1b76fd88de771db4a8edc
jatinnegi <negijatinn@gmail.com>
2023-05-30 00:03:18 +0530

    Add account verification service

--- src/utils/paths.ts
+++ src/utils/paths.ts
@@ -4,6 +4,6 @@ export const joinRelativeToMainPath = (path = "") => {
   const { filename } = require.main || {};
   console.log(filename);
   if (!filename) return path;
-  console.log(dirname(filename), path);
+  console.log(join(dirname(filename), path));
   return join(dirname(filename), path);
 };
